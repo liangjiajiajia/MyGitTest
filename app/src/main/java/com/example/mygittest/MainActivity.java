2f51d9f3841fefdcde1b5eff6c128516eaceab4e
liangjiajiajia <38244513+liangjiajiajia@users.noreply.github.com>
2021-08-20 17:29:36 +0800

    增加log

--- app/src/main/java/com/example/mygittest/MainActivity.java
+++ app/src/main/java/com/example/mygittest/MainActivity.java
@@ -3,6 +3,7 @@ package com.example.mygittest;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -13,5 +14,7 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         Toast.makeText(this, "dddd", Toast.LENGTH_SHORT).show();
+
+        Log.e("TAG", "onCreate: ");
     }
 }
